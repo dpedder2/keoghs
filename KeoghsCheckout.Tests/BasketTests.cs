11c013ab58640633376817c5cbcf6cb974c78ac8
Dan Pedder <hello@danielpedder.com>
2021-02-07 17:10:20 +0000

    Get the total basket cost for promo and none promo

--- KeoghsCheckout.Tests/BasketTests.cs
+++ KeoghsCheckout.Tests/BasketTests.cs
@@ -8,11 +8,6 @@ namespace KeoghsCheckout.Tests
     [TestFixture]
     public class BasketTests
     {
-        /*
-         * 
-         * BasketTotalCost_ItemsAddedWithAndWithoutPromotions_CorrectTotalCost
-         */
-        
         [Test]
         public void BasketAddItem_Item_AddsItem()
         {
@@ -125,6 +120,58 @@ namespace KeoghsCheckout.Tests
             }));
             
             Assert.AreEqual(207.5m, basket.GetTotalCost());
+        }
+
+        [Test]
+        public void BasketTotalCost_ItemsAddedWithAndWithoutPromotions_CorrectTotalCost()
+        {
+            
+            Basket basket = new Basket();
+            basket.AddItem(new Item("A", 10));
+            basket.AddItem(new Item("A", 10));
+            basket.AddItem(new Item("B", 15));
+            basket.AddItem(new Item("B", 15));
+            basket.AddItem(new Item("B", 15));
+            basket.AddItem(new Item("B", 15));
+            basket.AddItem(new Item("B", 15));
+            basket.AddItem(new Item("B", 15));
+            basket.AddItem(new Item("B", 15));
+            basket.AddItem(new Item("C", 40));
+            basket.AddItem(new Item("C", 40));
+            basket.AddItem(new Item("D", 55));
+            basket.AddItem(new Item("D", 55));
+            basket.AddItem(new Item("D", 55));
+            basket.AddItem(new Item("D", 55));
+            basket.AddItem(new Item("D", 55));
+            
+            basket.AddPromotion(new Promotion("B", "3 for 40", (quantity, unitPrice) =>
+            {
+                var total = 0.0m;
+                while (quantity >= 3)
+                {
+                    total += 40;
+                    quantity -= 3;
+                }
+            
+                total += quantity * unitPrice;
+                return total;
+            }));
+            
+            
+            basket.AddPromotion(new Promotion("D", "25% off for every 2 purchased together", (quantity, unitPrice) =>
+            {
+                var total = 0.0m;
+                while (quantity >= 2)
+                {
+                    total += ((2 * unitPrice) * 0.75m);
+                    quantity -= 2;
+                }
+            
+                total += quantity * unitPrice;
+                return total;
+            }));
+            
+            Assert.AreEqual(415m, basket.GetTotalCost());
         }
     }
 }
